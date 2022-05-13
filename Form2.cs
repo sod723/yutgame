8a28f5478cd629a2444bda8b8ca84d6ed958e05e
sod723 <81648520+sod723@users.noreply.github.com>
2022-05-14 00:06:05 +0900

    대각선 백도 구현

---
+++ Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yutgame
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+
+        }
+
+
+    }
+}
